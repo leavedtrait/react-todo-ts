42a03a452a34b2e528ed65a2194ac7ce03e083a1
leavedtrait <leavedtrait@gmail.com>
2024-06-23 17:04:12 +0300

    added form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import TodoItem from "./components/Todo";
+import TodoForm from "./components/TodoForm";
 import { dummyData } from "./data/todo";
 
 export default function App() {
   return (
-    <main className="py-10 h-screen w-full space-y-2 flex items-center flex-col text-slate-50 ">
+    <main className="py-10 h-screen w-full space-y-3 flex items-center flex-col text-slate-50 ">
       <h1 className=" font-bold text-3xl ">Your Todos</h1>
-      <div className="max-w-xl mx-auto flex ">
+      <div className="max-w-xl mx-auto flex flex-col space-y-3  ">
+        <div className="flex flex-col items-center">
+          <TodoForm />
+        </div>
         <div className=" space-y-2  flex-col items-center justify-center " >
           {dummyData.map(
             todo => (
---
+++ src/components/TodoForm.tsx
@@ -0,0 +1,13 @@
+export default function TodoForm(){
+    return(
+        <div>
+            <form action="" className=" space-x-3 flex ">
+                <input type="submit" value="" placeholder="Enter a todo" className=" border border-blue-500 w-40 rounded-md " />
+                <button type="submit">
+                    Add
+                </button>
+            </form>
+        </div>
+    )
+
+}
