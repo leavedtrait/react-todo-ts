13f8c93de906a1919724b96f84ce8644d40e2980
leavedtrait <leavedtrait@gmail.com>
2024-06-23 17:14:21 +0300

    modified:   src/components/TodoForm.tsx

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,8 +1,11 @@
+import { useState } from "react"
+
 export default function TodoForm(){
+    const [input,setInput] = useState("");
     return(
         <div>
             <form action="" className=" space-x-3 flex ">
-                <input type="submit" value="" placeholder="Enter a todo" className=" border border-blue-500 w-40 rounded-md " />
+                <input type="submit" value={input} placeholder="Enter a todo" className=" border border-blue-500 w-40 rounded-md " />
                 <button type="submit">
                     Add
                 </button>
