e9309c22bb22bb0dbfe612650292c31f45fc10fe
leavedtrait <leavedtrait@gmail.com>
2024-06-23 16:40:07 +0300

    core functionality implemented

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { dummyData } from "./data/todo";
 
 export default function App() {
   return (
-    <main className="py-10 h-screen w-full space-y-2 flex items-center flex-col ">
+    <main className="py-10 h-screen w-full space-y-2 flex items-center flex-col text-slate-50 ">
       <h1 className=" font-bold text-3xl ">Your Todos</h1>
       <div className="max-w-xl mx-auto flex ">
         <div className=" space-y-2  flex-col items-center justify-center " >
--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Todo } from "../types/todos";
 
 type todoProps = {
@@ -5,14 +6,27 @@ type todoProps = {
 }
 
 export default function TodoItem({ todo }: todoProps) {
+   
+    const [checked, setChecked] = useState(false);
+    const handleChange = () => {
+        setChecked(!checked)
+        todo.completed =checked
+    };
     return (
-        
-            <div className="flex items-center justify-center" >
-                
-                <p key={todo.id} className="text-lg  ">
-                    {todo.title}
-                </p>
+
+        <div className="" >
+            <div className="flex items-center space-x-2">
+            <input type="checkbox" onChange={handleChange} />
+
+            <p  key={todo.id}  className="text-lg">
+                {checked ? (
+                    <span className="line-through">{todo.title}</span>
+                ) : (
+                    <span> {todo.title}</span>
+                )}
+            </p>
             </div>
+        </div>
     )
 }
 
